2bf611149bf1969600b11fca53c27457070085d9
frankfurillo <gunnarfranklin@Gunnar-Franklins-MacBook-Pro.local>
2014-08-24 07:58:53 +0200

    perfect tempo implementation

--- jQuencer.js
+++ jQuencer.js
@@ -7,6 +7,9 @@ var jQuencer={
     commands: ["sequence", "single"],
     vca:null,
     st: "",
+    
+        
+    
     defaults : {
         maxFrequency: 4000
     },
@@ -62,6 +65,22 @@ var jQuencer={
     }
     ,
     sequencer : {
+	    startTime:null,              // The start time of the entire sequence.
+	
+	    tempo :120.0,      	    // tempo (in beats per minute)
+		lookahead : 25.0,       // How frequently to call scheduling function 
+	                            //(in milliseconds)
+		scheduleAheadTime : 0.1,    // How far ahead to schedule audio (sec)
+	                            // This is calculated from lookahead, and overlaps 
+	                            // with next interval (in case the timer is late)
+		nextNoteTime : 0.0,     // when the next note is due.
+		 timerID : 0,            // setInterval identifier.
+	
+	    notesInQueue : [],      // the notes that have been put into the web audio,
+	
+
+    
+    
         stepStarted:$.Event("StepStarted"),
         stepStopped: $.Event("StepStopped"),
         currentIndex: 0,
@@ -109,6 +128,43 @@ var jQuencer={
         removeFromStep:function(stepIndex,oscillatorIndex){
 	      this.steps[stepIndex].oscillators.splice(oscillatorIndex,1);  
         },
+        
+        scheduler:function() {
+		    // while there are notes that will need to play before the next interval, 
+		    // schedule them and advance the pointer.
+		    while (this.nextNoteTime < jQuencer.ctx.currentTime + this.scheduleAheadTime ) {
+		        this.scheduleNote( this.currentIndex, this.nextNoteTime );
+		        this.nextNote();
+		    };
+		    this.timerID = window.setTimeout( function(){
+		    	jQuencer.sequencer.scheduler();
+		    }, this.lookahead );
+		},
+
+        nextNote:function() {
+		    // Advance current note and time by a 16th note...
+		    var secondsPerBeat = 60.0 / this.tempo;    // Notice this picks up the CURRENT 
+		                                          // tempo value to calculate beat length.
+		    this.nextNoteTime += 0.25 * secondsPerBeat;    // Add beat length to last beat time
+		
+		    this.currentIndex++;    // Advance the beat number, wrap to zero
+		    if (this.currentIndex== this.steps.length - 1) {
+		        this.currentIndex = 0;
+		    }
+		},
+		scheduleNote:function ( beatNumber, time ) {
+		    // push the note on the queue, even if we're not playing.
+		    this.notesInQueue.push( { note: beatNumber, time: time } );
+		
+		    // create an oscillator
+		    var osc = jQuencer.ctx.createOscillator();
+		    osc.connect( jQuencer.ctx.destination );
+		    osc.frequency.value = 220.0;
+		
+		    osc.start( time );
+		    osc.stop( time + 0.2 );
+		},
+
         timerCount:0,
 		playAllSteps: function () {
 			if (this.steps.length == 0) { return; }
@@ -211,11 +267,25 @@ var jQuencer={
         ,
         
         run: function () {
-            //this.stopPreviousStep();
+        
+            this.isPlaying = !this.isPlaying;
+		
+		    if (this.isPlaying) { // start playing
+		        this.currentIndex = 0;
+		        this.nextNoteTime = jQuencer.ctx.currentTime;
+		        this.scheduler();    // kick off scheduling
+		        return "stop";
+		    } else {
+		        window.clearTimeout( timerID );
+		        return "play";
+		    }
+
             //this.playAllSteps(); //UNDER CONSTRUCTION
 			//return;
-			this.playStep();
-            this.currentIndex++;
+		
+		
+			//this.playStep(); //working but bad shit
+            //this.currentIndex++;
 
 
             clearTimeout(this.st);
